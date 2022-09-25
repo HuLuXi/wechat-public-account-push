4974bb6ce73c9bba1cc297e1312b076c1d89cf0d
HuLuXi <38487534+HuLuXi@users.noreply.github.com>
2022-09-25 09:36:57 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -208,7 +208,7 @@ const USER_CONFIG = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	usf3hdbtJmX1rRT9FRN7pGbNozGaYA7qHDgZxkUMIV0',
+  CALLBACK_TEMPLATE_ID: 'usf3hdbtJmX1rRT9FRN7pGbNozGaYA7qHDgZxkUMIV0',
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
